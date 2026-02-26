d2f93f7fc0618a6eb2076cf5dd19b231cf843978
NachoMrt <josigmcs@gmail.com>
2026-02-26 10:09:19 +0100

    se añade citas

---
+++ Parte_1/models/citas.php
@@ -0,0 +1,19 @@
+<?php
+require_once "Conexion.php";
+
+class Cita extends Conexion {
+
+    public function insertar($id_medico, $dia, $hora, $nombre, $dni, $id_paciente) {
+        $sql = "INSERT INTO citas (Id_medico, dia, hora, nombre, DNI, id_paciente)
+                VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$id_medico, $dia, $hora, $nombre, $dni, $id_paciente]);
+    }
+
+    public function obtenerPorPaciente($id_paciente) {
+        $sql = "SELECT * FROM citas WHERE id_paciente = ?";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([$id_paciente]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
