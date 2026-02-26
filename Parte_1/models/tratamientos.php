2cefd5e21b4991cc718079849e402ecd46459ede
NachoMrt <josigmcs@gmail.com>
2026-02-26 10:11:27 +0100

    se añade tratamientos

---
+++ Parte_1/models/tratamientos.php
@@ -0,0 +1,19 @@
+<?php
+require_once "Conexion.php";
+
+class Tratamiento extends Conexion {
+
+    public function insertar($nombre, $duracion, $id_paciente) {
+        $sql = "INSERT INTO tratamientos (nombre, duracion, id_paciente)
+                VALUES (?, ?, ?)";
+        $stmt = $this->conexion->prepare($sql);
+        return $stmt->execute([$nombre, $duracion, $id_paciente]);
+    }
+
+    public function obtenerPorPaciente($id_paciente) {
+        $sql = "SELECT * FROM tratamientos WHERE id_paciente = ?";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([$id_paciente]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
